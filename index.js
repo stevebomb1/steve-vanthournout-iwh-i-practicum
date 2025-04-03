745bad2006d23ad6aa222a6fd0416886d527b5be
steve vanthournout <steve.vanthournout@provisionsgroup.com>
2025-04-02 19:00:48 -0500

    Submitting Route 2 and fixing homepage.pug

--- index.js
+++ index.js
@@ -32,7 +32,13 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cacti', (req, res) => {
+    try {
+      res.render('updates', { pageTitle: 'Update cacti Form | Integrating With HubSpot I Practicum' }); // Render the updates.pug template
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
